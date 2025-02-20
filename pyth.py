37b53c180984939f6f15d16ca3309eeb5569a412
sakshi222345 <sakshikpatel_bscibm2024@iar.ac.in>
2025-02-20 21:03:05 +0530

    Update pyth.py

--- pyth.py
+++ pyth.py
@@ -5,3 +5,4 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> sum = num1 + num2
 >>> print ('the sum of{0} and {1} is {2}'.format(num1, num2, sum))
 the sum of3 and 4 is 7
+#this is a code
