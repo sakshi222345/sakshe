1c64e3e5dd24e5677c80ebd49abd61c7514e6c68
sakshi222345 <sakshikpatel_bscibm2024@iar.ac.in>
2025-02-20 19:42:17 +0530

    Add files via upload

---
+++ pyth.py
@@ -0,0 +1,7 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = 3
+>>> num2 = 4
+>>> sum = num1 + num2
+>>> print ('the sum of{0} and {1} is {2}'.format(num1, num2, sum))
+the sum of3 and 4 is 7
